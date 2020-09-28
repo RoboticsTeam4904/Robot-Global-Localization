74afbc7b94125916d23011550d8516f563d3c7a1
countableclouds <aksharramkumar@gmail.com>
2020-09-27 17:42:45 -0700

    Kalman Filter Trait

--- src/ai/kalman_filter.rs
+++ src/ai/kalman_filter.rs
@@ -1,169 +1,162 @@
-use crate::utility::Pose;
+use abomonation::Abomonation;
 use generic_array::ArrayLength;
-use nalgebra::base::allocator::Allocator;
-use nalgebra::base::default_allocator::DefaultAllocator;
-use nalgebra::base::dimension::{Dim, DimAdd, DimMul, DimName, DimNameAdd, DimNameMul};
-use nalgebra::{ArrayStorage, Matrix, Matrix6, RowVector6, U1, U13, U6};
-use std::f64::consts::*;
+use nalgebra::{
+    base::allocator::Allocator, ArrayStorage, DefaultAllocator, DimName, DimNameAdd, DimNameMul,
+    Matrix, Matrix3, Matrix6, RowVector3, RowVector6, U1,
+};
 use std::ops::Mul;
 use typenum::operator_aliases::Prod;
 
-pub struct Config {
-    alpha: f64,
-    beta: f64,
-    kappa: f64,
-}
+pub type GeneralMatrix<D1, D2> = Matrix<f64, D1, D2, ArrayStorage<f64, D1, D2>>;
+pub type SquareMatrix<D1> = GeneralMatrix<D1, D1>;
+pub type RowVector<D1> = GeneralMatrix<U1, D1>;
 
-pub trait KalmanFilter<A, B, C, D, E, F, G>
+pub trait KalmanFilter<D1, D2>
 where
-    A: DimName,
-    B: DimName,
-    A::Value: Mul<B::Value>,
-    Prod<A::Value, B::Value>: ArrayLength<f64>,
-    <<A as nalgebra::base::dimension::DimName>::Value as std::ops::Mul<
-        <B as nalgebra::base::dimension::DimName>::Value,
-    >>::Output: generic_array::ArrayLength<f64>,
+    D1: DimName + DimNameMul<D2> + DimNameAdd<D2>,
+    D2: DimName + DimNameMul<D1> + DimNameAdd<D1>,
+    D1::Value: Mul<D2::Value>,
+    Prod<D1::Value, D2::Value>: ArrayLength<f64>,
+    DefaultAllocator: Allocator<f64, D1, D2>,
 {
-    type StateDimension: DimName;
-    type SensorDimension: DimName;
-    fn control_sigma_matrix(&self) -> Matrix<f64, A, B, ArrayStorage<f64, A, B>>;
-
-    // fn set_control_sigma_matrix(
-    //     &self,
-    //     control_sigma_matrix: Matrix<
-    //         f64,
-    //         Self::State2x1Dimension,
-    //         Self::ControlDimension,
-    //         ArrayStorage<f64, Self::State2x1Dimension, Self::ControlDimension>,
-    //     >,
-    // );
-
-    // fn sensor_sigma_matrix(
-    //     &self,
-    // ) -> Matrix<
-    //     f64,
-    //     Self::State2x1Dimension,
-    //     Self::SensorDimension,
-    //     ArrayStorage<f64, Self::State2x1Dimension, Self::SensorDimension>,
-    // >;
-
-    // fn known_state(
-    //     &self,
-    // ) -> Matrix<f64, Self::StateDimension, U1, ArrayStorage<f64, Self::StateDimension, U1>>;
-
-    // fn set_known_state(
-    //     &self,
-    //     set_matrix: Matrix<
-    //         f64,
-    //         Self::StateDimension,
-    //         U1,
-    //         ArrayStorage<f64, Self::StateDimension, U1>,
-    //     >,
-    // );
-
-    // fn config(&self) -> Config;
-
-    // fn covariance_matrix(
-    //     &self,
-    // ) -> Matrix<
-    //     f64,
-    //     Self::StateDimension,
-    //     Self::StateDimension,
-    //     ArrayStorage<f64, Self::StateDimension, Self::StateDimension>,
-    // >;
-
-    // fn update_sigma_matrix(&mut self);
-
-    // fn control(
-    //     control_sigma_matrix: Matrix<
-    //         f64,
-    //         Self::State2x1Dimension,
-    //         Self::ControlDimension,
-    //         ArrayStorage<f64, Self::State2x1Dimension, Self::ControlDimension>,
-    //     >,
-    //     control: Matrix<
-    //         f64,
-    //         Self::ControlDimension,
-    //         U1,
-    //         ArrayStorage<f64, Self::ControlDimension, U1>,
-    //     >,
-    //     time: f64,
-    // ) -> Matrix<
-    //     f64,
-    //     Self::State2x1Dimension,
-    //     Self::ControlDimension,
-    //     ArrayStorage<f64, Self::State2x1Dimension, Self::ControlDimension>,
-    // > {
-    //     control_sigma_matrix.row_iter_mut().for_each(|mut e| {
-    //         e[1] += e[4] * time;
-    //         e[2] += e[5] * time;
-    //         e[4] += (control.position.x * e[0].cos()
-    //             + control.position.y * (e[0] - FRAC_PI_2).cos())
-    //             * time;
-    //         e[5] += (control.position.x * e[0].sin()
-    //             + control.position.y * (e[0] - FRAC_PI_2).sin())
-    //             * time;
-    //         e[0] = (e[0] + e[3] * time) % (2. * PI);
-    //         e[3] += control.angle * time;
-    //     });
-    //     control_sigma_matrix
-    // }
-
-    // fn sense();
-
-    // fn prediction_update(
-    //     &mut self,
-    //     time: f64,
-    //     control: Matrix<
-    //         f64,
-    //         Self::ControlDimension,
-    //         U1,
-    //         ArrayStorage<f64, Self::ControlDimension, U1>,
-    //     >,
-    // ) {
-    //     self.gen_sigma_matrix();
-
-    //     self.set_control_sigma_matrix(control(&mut self.control_sigma_matrix(), control));
-    //     // self.control_sigma_matrix().column(4) = Vector13::from_element(mult_pose.position.x);
-    //     let lambda = (self.config().alpha.powi(2)) * (6. + self.config().kappa) - 6.;
-    //     self.set_known_state(RowVector6::from_element(0.));
-    //     for i in 0..=(2 * 6) {
-    //         self.set_known_state(
-    //             self.known_state()
-    //                 + self.control_sigma_matrix().row(i) / (6. + lambda)
-    //                     * if i != 0 { 1. / 2. } else { lambda },
-    //         );
-    //     }
-    //     let temp_sigma_matrix =
-    //         self.control_sigma_matrix() - Matrix::from_rows(&vec![self.known_state(); 13]);
-    //     self.covariance_matrix() = Matrix::from_element(0.);
-    //     for i in 0..=(2 * 6) {
-    //         self.set_covariance_matrix(
-    //             self.covariance_matrix()
-    //                 + temp_sigma_matrix.row(i).transpose()
-    //                     * temp_sigma_matrix.row(i)
-    //                     * if i == 0 {
-    //                         lambda / (6. + lambda)
-    //                             + (1. - self.config().alpha.powi(2) + self.config().beta)
-    //                     } else {
-    //                         1. / (2. * (6. + lambda))
-    //                     },
-    //         );
-    //     }
-    //     self.set_covariance_matrix(self.q() + self.covariance_matrix());
-    // }
-
-    // fn measurement_update(
-    //     &mut self,
-    //     sensor_data: Matrix<
-    //         f64,
-    //         Self::SensorDimension,
-    //         U1,
-    //         ArrayStorage<f64, Self::SensorDimension, U1>,
-    //     >,
-    // ) {
-    // }
+    fn control_sigma_matrix(&self) -> GeneralMatrix<D1, D2>;
 }
+
+// fn set_control_sigma_matrix(
+//     &self,
+//     control_sigma_matrix: Matrix<
+//         f64,
+//         Self::State2x1Dimension,
+//         Self::ControlDimension,
+//         ArrayStorage<f64, Self::State2x1Dimension, Self::ControlDimension>,
+//     >,
+// );
+
+// fn sensor_sigma_matrix(
+//     &self,
+// ) -> Matrix<
+//     f64,
+//     Self::State2x1Dimension,
+//     Self::SensorDimension,
+//     ArrayStorage<f64, Self::State2x1Dimension, Self::SensorDimension>,
+// >;
+
+// fn known_state(
+//     &self,
+// ) -> Matrix<f64, Self::StateDimension, U1, ArrayStorage<f64, Self::StateDimension, U1>>;
+
+// fn set_known_state(
+//     &self,
+//     set_matrix: Matrix<
+//         f64,
+//         Self::StateDimension,
+//         U1,
+//         ArrayStorage<f64, Self::StateDimension, U1>,
+//     >,
+// );
+
+// fn config(&self) -> Config;
+
+// fn covariance_matrix(
+//     &self,
+// ) -> Matrix<
+//     f64,
+//     Self::StateDimension,
+//     Self::StateDimension,
+//     ArrayStorage<f64, Self::StateDimension, Self::StateDimension>,
+// >;
+
+// fn update_sigma_matrix(&mut self);
+
+// fn control(
+//     control_sigma_matrix: Matrix<
+//         f64,
+//         Self::State2x1Dimension,
+//         Self::ControlDimension,
+//         ArrayStorage<f64, Self::State2x1Dimension, Self::ControlDimension>,
+//     >,
+//     control: Matrix<
+//         f64,
+//         Self::ControlDimension,
+//         U1,
+//         ArrayStorage<f64, Self::ControlDimension, U1>,
+//     >,
+//     time: f64,
+// ) -> Matrix<
+//     f64,
+//     Self::State2x1Dimension,
+//     Self::ControlDimension,
+//     ArrayStorage<f64, Self::State2x1Dimension, Self::ControlDimension>,
+// > {
+//     control_sigma_matrix.row_iter_mut().for_each(|mut e| {
+//         e[1] += e[4] * time;
+//         e[2] += e[5] * time;
+//         e[4] += (control.position.x * e[0].cos()
+//             + control.position.y * (e[0] - FRAC_PI_2).cos())
+//             * time;
+//         e[5] += (control.position.x * e[0].sin()
+//             + control.position.y * (e[0] - FRAC_PI_2).sin())
+//             * time;
+//         e[0] = (e[0] + e[3] * time) % (2. * PI);
+//         e[3] += control.angle * time;
+//     });
+//     control_sigma_matrix
+// }
+
+// fn sense();
+
+// fn prediction_update(
+//     &mut self,
+//     time: f64,
+//     control: Matrix<
+//         f64,
+//         Self::ControlDimension,
+//         U1,
+//         ArrayStorage<f64, Self::ControlDimension, U1>,
+//     >,
+// ) {
+//     self.gen_sigma_matrix();
+
+//     self.set_control_sigma_matrix(control(&mut self.control_sigma_matrix(), control));
+//     // self.control_sigma_matrix().column(4) = Vector13::from_element(mult_pose.position.x);
+//     let lambda = (self.config().alpha.powi(2)) * (6. + self.config().kappa) - 6.;
+//     self.set_known_state(RowVector6::from_element(0.));
+//     for i in 0..=(2 * 6) {
+//         self.set_known_state(
+//             self.known_state()
+//                 + self.control_sigma_matrix().row(i) / (6. + lambda)
+//                     * if i != 0 { 1. / 2. } else { lambda },
+//         );
+//     }
+//     let temp_sigma_matrix =
+//         self.control_sigma_matrix() - Matrix::from_rows(&vec![self.known_state(); 13]);
+//     self.covariance_matrix() = Matrix::from_element(0.);
+//     for i in 0..=(2 * 6) {
+//         self.set_covariance_matrix(
+//             self.covariance_matrix()
+//                 + temp_sigma_matrix.row(i).transpose()
+//                     * temp_sigma_matrix.row(i)
+//                     * if i == 0 {
+//                         lambda / (6. + lambda)
+//                             + (1. - self.config().alpha.powi(2) + self.config().beta)
+//                     } else {
+//                         1. / (2. * (6. + lambda))
+//                     },
+//         );
+//     }
+//     self.set_covariance_matrix(self.q() + self.covariance_matrix());
+// }
+
+// fn measurement_update(
+//     &mut self,
+//     sensor_data: Matrix<
+//         f64,
+//         Self::SensorDimension,
+//         U1,
+//         ArrayStorage<f64, Self::SensorDimension, U1>,
+//     >,
+// ) {
+// }
 
 // /// Uses Unscented Kalman Filter to approximate robot state
 // pub struct KalmanFilter {
